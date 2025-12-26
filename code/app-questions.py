5f2c69da0db9b71103b116a14c17e56b8930151d
hssboston <hanna.s.suzuki@gmail.com>
2025-12-26 07:41:45 -0500

    Update app-questions.py

--- code/app-questions.py
+++ code/app-questions.py
@@ -7,7 +7,7 @@ listSharpOrFlat = ["sharps", "flats"]
 accidentals = random.choice(listNumOfAccidentals)
 sharpOrFlat = random.choice(listSharpOrFlat)
 
-print("What is the name of the key signature with "+str(accidentals)+" "+sharpOrFlat)
+print("What is the name of the key signature with " + str(accidentals) + " " + sharpOrFlat)
 
 if sharpOrFlat == "sharps":
     ks = key.KeySignature(accidentals);
@@ -18,12 +18,10 @@ else:
     key = ks.asKey()
     print(key)
     
-print("What is the relative minor of "+str(key))
-
+print("What is the relative minor of " + str(key))
 print(key.relative)
 
-print("What is the parallel minor of "+str(key))
-
+print("What is the parallel minor of " + str(key))
 print(key.parallel)
 
 
