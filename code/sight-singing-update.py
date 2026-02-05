1d48bb8c3b7161ef24b7c0f88070027592eef9e1
hssboston <hanna.s.suzuki@gmail.com>
2026-02-05 11:05:50 -0500

    Update sight-singing-update.py

--- code/sight-singing-update.py
+++ code/sight-singing-update.py
@@ -4,27 +4,29 @@ import random, numpy as np
 RANDOM_SEED_NOTE = None #None or int
 rng = np.random.default_rng(seed=RANDOM_SEED_NOTE)
 
+# Pitch-to-pitch transition matrix
 P = np.array([
-    [0,   0.4,  0.4, 0.2,  0, 0,    0,    0],
+    [0,   0.4,  0.4, 0.2,  0,   0,    0,    0],
     [0.3, 0,    0.4, 0.3,  0,   0,    0,    0],
     [0.1, 0.4,  0,   0.4,  0.1, 0,    0,    0],
     [0,   0.15, 0.4, 0,    0.4, 0.05, 0,    0],
     [0,   0,    0.2, 0.65, 0,   0.1,  0.05, 0],
     [0,   0,    0,   0.3,  0.5, 0,    0.2,  0],
     [0,   0,    0,   0.1,  0.1, 0.3,  0,    0.5],
-    [0,   0,    0,   0,    0.6,   0,  0.4,  0]])
+    [0,   0,    0,   0,    0.6, 0,    0.4,  0]])
 
-#SIMPLE 4/4
+# Rhythm patterns for the 1st measure when in 4/4 (FF) time
 measureOneFF = [
     [1, 1,   0.5, 0.5, 1],
     [1, 1,   1,   0.5, 0.5],
-    [1, 0.5, 0.5, 1, 1],
+    [1, 0.5, 0.5, 1,   1],
     [1, 1,   1,   1],
     [1, 1,   0.5, 0.5, 0.5, 0.5],
     [1, 0.5, 0.5, 0.5, 0.5, 1]]
 
+# Rhythm patterns for the 2nd and 3rd measures when in 4/4 (FF) time
 measureTwoThreeFF = [
-    [0.5, 0.5, 1, 1.5,   0.5],
+    [0.5, 0.5, 1,   1.5, 0.5],
     [1.5, 0.5, 2],
     [1.5, 0.5, 1,   0.5, 0.5],
     [1,   1,   1.5, 0.5],
@@ -36,12 +38,13 @@ measureTwoThreeFF = [
     [0.5, 0.5, 1,   1,   0.5, 0.5],
     [0.5, 0.5, 0.5, 0.5, 1,   1]]
 
+# Rhythm patterns for the 4th measure when in 4/4 (FF) time
 measureFourFF = [
     [1,   1,   2],
-    [0.5, 0.5, 1, 2],
+    [0.5, 0.5, 1,   2],
     [1,   0.5, 0.5, 2]]
 
-#SIMPLE 6/8
+# Rhythm patterns for the 1st measure when in 6/8 (SE) time
 measureOneSE = [
     [0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
     [0.5, 0.5, 0.5, 1,   0.5],
@@ -52,56 +55,64 @@ measureOneSE = [
     [0.5, 1,   0.5, 1],
     [1,   0.5, 0.5, 1]]
 
+# Rhythm patterns for the 2nd and 3rd measures when in 6/8 (SE) time
 measureTwoThreeSE = [
-    [0.75, 0.25, 0.5,  0.5,   0.5,  0.5],
-    [0.75, 0.25, 0.5,  1,     0.5],
-    [0.75, 0.25, 0.5,  0.5,   1],
-    [0.5,  0.5,  0.5,  0.75,  0.25, 0.5],
-    [0.5,  1,    0.75, 0.25,  0.5],
-    [1,    0.5,  0.75, 0.25,  0.5],
-    [0.5,  0.5,  0.5,  1,     0.5],
-    [0.5,  0.5,  0.5,  0.5,   1],
-    [1,    0.5,  0.5,  0.5,   0.5],
-    [0.5,  1,    0.5,  0.5,   0.5],
+    [0.75, 0.25, 0.5,  0.5,  0.5,  0.5],
+    [0.75, 0.25, 0.5,  1,    0.5],
+    [0.75, 0.25, 0.5,  0.5,  1],
+    [0.5,  0.5,  0.5,  0.75, 0.25, 0.5],
+    [0.5,  1,    0.75, 0.25, 0.5],
+    [1,    0.5,  0.75, 0.25, 0.5],
+    [0.5,  0.5,  0.5,  1,    0.5],
+    [0.5,  0.5,  0.5,  0.5,  1],
+    [1,    0.5,  0.5,  0.5,  0.5],
+    [0.5,  1,    0.5,  0.5,  0.5],
     [1,    0.5,  1,    0.5],
     [0.5,  1,    0.5,  1],
     [1,    0.5,  0.5,  1]]
 
+# Rhythm patterns for the 4th measure when in 6/8 (SE) time
 measureFourSE = [
-    [0.5, 0.5, 0.5,  1.5],
+    [0.5, 0.5, 0.5, 1.5],
     [1,   0.5, 1.5],
     [0.5, 1,   1.5]]
 
-
-#up to three accidentals only (to keep problems not too hard)
-keyLettersList = ["C","G","D","A","F","B-","E-","a","e","b", "d", "g", "c"]
+# Key selection
+keyLettersList = ["C","G","D","A","F","B-","E-",
+                  "a","e","b", "d", "g", "c"]
 keyLetter = random.choice(keyLettersList)
 k = key.Key(keyLetter)
+print("Key:", k)
 
 if k.mode == "major":
   sc = scale.MajorScale(keyLetter)
 else:
   sc = scale.HarmonicMinorScale(keyLetter)
 
-scalePitchNames = []
+# Clef selection
+scalePitchNames = [] # Pitch names with an octave number ("C4") in the selected key
 
-randomInt = random.randint(0,1)
-if randomInt < 0.5:
+randomFloat = random.random()
+if randomFloat < 0.5:
     cl = clef.TrebleClef()
     for pitch in sc.getPitches(keyLetter+"4"):
-        scalePitchNames.append(pitch.nameWithOctave)
-    
+        scalePitchNames.append(pitch.nameWithOctave)    
 else:
     cl = clef.BassClef()
     for pitch in sc.getPitches(keyLetter+"3"):
         scalePitchNames.append(pitch.nameWithOctave)
+print("Clef:",    cl)
+print("Scale pitch names", scalePitchNames)
 
-randomInt = random.randint(0,1)
-if randomInt < 0.5:
+# Time signature selection
+randomFloat = random.random()
+if randomFloat < 0.5:
     timeSig = "4/4"
 else:
     timeSig = "6/8"
+print("Time signature:", timeSig)
 
+# Score initialization
 melody = stream.Part()
 m1 = stream.Measure()
 m1.append(cl)
@@ -113,7 +124,6 @@ m4 = stream.Measure()
 m4.rightBarLine = bar.Barline("final")
 
 if timeSig == "4/4":
-    #LIST OF STRINGS
     m1Rhythm = random.choice(measureOneFF)
     m2Rhythm = random.choice(measureTwoThreeFF)
     m3Rhythm = random.choice(measureTwoThreeFF)
@@ -123,124 +133,105 @@ else:
     m2Rhythm = random.choice(measureTwoThreeSE)
     m3Rhythm = random.choice(measureTwoThreeSE)
     m4Rhythm = random.choice(measureFourSE)
+print("Rhythm pattern for measure 1:", m1Rhythm)
+print("Rhythm pattern for measure 2:", m2Rhythm)
+print("Rhythm pattern for measure 3:", m3Rhythm)
+print("Rhythm pattern for measure 4:", m4Rhythm)
 
-print(m1Rhythm)
-print(m2Rhythm)
-print(m3Rhythm)
-print(m4Rhythm)
+# adjust the octave of a new note by prioritizing stepwise melodic motion
+def adjustOctave(newNote, prevNote):
+    if (newNote.pitch.midi - prevNote.pitch.midi) >= 7:
+        newNote.octave -= 1
+        print("Octave adjustment. SD", newNote.octave+1, "-> SD", newNote.octave, end="")
+        return True
+    if (prevNote.pitch.midi - newNote.pitch.midi) >= 7:
+        newNote.octave += 1
+        print("Octave adjustment. SD", newNote.octave-1, "-> SD", newNote.octave, end="")
+        return True
+
+def harmonicMinor(newNote, newNoteSD, keyLetter):
+    if newNoteSD == "7" and keyLetter.islower():
+        newNote.pitch.accidental("sharp")
+        print("Harmonic minor. Sharpened", newNote, end="")
 
 prevNote = None
 
-#MEASURE ONE
+# Measure 1
 for index, noteDuration in enumerate(m1Rhythm):
     if index == 0:
         newNote = note.Note(k.tonic)
-        newNoteSD = "1"
+        newNote.quarterLength = noteDuration
         if cl == clef.BassClef():
             newNote.octave = 3
         else:
             newNote.octave = 4
-        newNote.quarterLength = noteDuration
     else:
-        print(prevNote.nameWithOctave)
-        newNoteSD = rng.choice(["1", "2", "3", "4", "5", "6", "7", "8"], p=P[scalePitchNames.index(prevNote.nameWithOctave)])
+        newNoteSD = rng.choice(["1", "2", "3", "4", "5", "6", "7", "8"],
+                               p=P[scalePitchNames.index(prevNote.nameWithOctave)])
         newNote = note.Note(scalePitchNames[int(newNoteSD)-1])
-        
-        newNote.octave = m1.notes.first().octave
-        if (newNote.pitch.midi - prevNote.pitch.midi) >= 7:
-            newNote.octave -= 1
-        if (prevNote.pitch.midi - newNote.pitch.midi) >= 7:
-            newNote.octave += 1
-            
         newNote.quarterLength = noteDuration
+        newNote.octave = m1.notes.first().octave        
 
+        if adjustOctave(newNote, prevNote):
+            print(" Measure 1, Note index", index)
+        if harmonicMinor(newNote, newNoteSD, keyLetter):
+            print(" Measure 1, Note index", index)
     prevNote = newNote
-    
     m1.append(newNote)
-    
-    
-    
-#MEASURE TWO
-for index, noteDuration in enumerate(m2Rhythm):
-    print(prevNote.nameWithOctave)
-    newNoteSD = rng.choice(["1", "2", "3", "4", "5", "6", "7", "8"], p=P[scalePitchNames.index(prevNote.nameWithOctave)])
-    newNote = note.Note(scalePitchNames[int(newNoteSD)-1])
-    
-    
-    newNote.octave = m1.notes.first().octave
-#     if (newNote.pitch.midi - prevNote.pitch.midi) > 7:
-#         testNewNote = newNote
-#         testNewNote.octave -= 1
-#         if (testNewNote.pitch.midi - prevNote.pitch.midi) > (newNote.pitch.midi - prevNote.pitch.midi):
-#             pass
-#         else:
-#             newNote.octave -= 1
-#     if (prevNote.pitch.midi - newNote.pitch.midi) > 7:
-#         testNewNote = newNote
-#         testNewNote.octave += 1
-#         if (prevNote.pitch.midi - testNewNote.pitch.midi) > (prevNote.pitch.midi - newNote.pitch.midi):
-#             pass
-#         else:
-#             newNote.octave += 1
-    if (newNote.pitch.midi - prevNote.pitch.midi) >= 7:
-        newNote.octave -= 1
-    if (prevNote.pitch.midi - newNote.pitch.midi) >=7:
-        newNote.octave += 1
-    
-    newNote.quarterLength = noteDuration
 
+# Measure 2
+for index, noteDuration in enumerate(m2Rhythm):
+    newNoteSD = rng.choice(["1", "2", "3", "4", "5", "6", "7", "8"],
+                           p=P[scalePitchNames.index(prevNote.nameWithOctave)])
+    newNote = note.Note(scalePitchNames[int(newNoteSD)-1])
+    newNote.quarterLength = noteDuration
+    newNote.octave = m1.notes.first().octave
+    if adjustOctave(newNote, prevNote):
+        print(" Measure 2, Note index", index)
+    if harmonicMinor(newNote, newNoteSD, keyLetter):
+        print(" Measure 2, Note index", index)
     prevNote = newNote
-    
     m2.append(newNote)
     
-#MEASURE THREE
+# Measure 3
 for index, noteDuration in enumerate(m3Rhythm):
-    print(prevNote.nameWithOctave)
-    newNoteSD = rng.choice(["1", "2", "3", "4", "5", "6", "7", "8"], p=P[scalePitchNames.index(prevNote.nameWithOctave)])
+    newNoteSD = rng.choice(["1", "2", "3", "4", "5", "6", "7", "8"],
+                           p=P[scalePitchNames.index(prevNote.nameWithOctave)])
     newNote = note.Note(scalePitchNames[int(newNoteSD)-1])
-    
-    newNote.octave = m1.notes.first().octave
-    if (newNote.pitch.midi - prevNote.pitch.midi) >= 7:
-        newNote.octave -= 1
-    if (prevNote.pitch.midi - newNote.pitch.midi) >= 7:
-        newNote.octave += 1
-        
     newNote.quarterLength = noteDuration
-
+    newNote.octave = m1.notes.first().octave
+    if adjustOctave(newNote, prevNote):
+        print(" Measure 3, Note index", index)
+    if harmonicMinor(newNote, newNoteSD, keyLetter):
+        print(" Measure 3, Note index", index)
     prevNote = newNote
-        
     m3.append(newNote)
 
-#MEASURE FOUR
+# Measure 4
 for index, noteDuration in enumerate(m4Rhythm):
     if index == len(m4Rhythm) - 1:
         newNote = note.Note(k.tonic)
-        newNoteSD = "1"
         if cl == clef.BassClef():
             newNote.octave = 3
         else:
             newNote.octave = 4
         newNote.quarterLength = noteDuration
     else:
-        print(prevNote.nameWithOctave)
-        newNoteSD = rng.choice(["1", "2", "3", "4", "5", "6", "7", "8"], p=P[scalePitchNames.index(prevNote.nameWithOctave)])
+        newNoteSD = rng.choice(["1", "2", "3", "4", "5", "6", "7", "8"],
+                               p=P[scalePitchNames.index(prevNote.nameWithOctave)])
         newNote = note.Note(scalePitchNames[int(newNoteSD)-1])
-                
-        newNote.octave = m1.notes.first().octave
-        if (newNote.pitch.midi - prevNote.pitch.midi) >= 7:
-            newNote.octave -= 1
-        if (prevNote.pitch.midi - newNote.pitch.midi) >= 7:
-            newNote.octave += 1
-            
         newNote.quarterLength = noteDuration
-
+        newNote.octave = m1.notes.first().octave
+        if adjustOctave(newNote, prevNote):
+            print(" Measure 4, Note index", index)
+        if harmonicMinor(newNote, newNoteSD, keyLetter):
+            print(" Measure 4, Note index", index)
     prevNote = newNote
-    
     m4.append(newNote)
 
 melody.append(m1)
 melody.append(m2)
 melody.append(m3)
 melody.append(m4)
-melody.show("text")
-melody.show()
+# melody.show("text")
+# melody.show()
